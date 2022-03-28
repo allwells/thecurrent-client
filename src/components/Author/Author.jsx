0aae7f127c2a32e09b3945d4504500517821ff2b
Allwell Onen <aleenfestus@gmail.com>
2022-03-28 06:51:27 +0100

    [Update]: Display authors of post
    Signed-off-by: Allwell Onen <aleenfestus@gmail.com>

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -5,8 +5,8 @@ import React from "react";
 const Author = ({ blog }) => {
   return (
     <div className="author">
-      <span className="author-label">Author: </span>
-      <span className="author-name">Chisom Emmanual</span>
+      <span className="author-label">Author(s): </span>
+      <span className="author-name">{blog.author}</span>
     </div>
   );
 };
