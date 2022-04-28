9d99a8ed334dea9348c46fa5e16fa0e35e7aceca
Allwell Onen <aleenfestus@gmail.com>
2022-04-28 04:04:27 +0100

    Rebuild about page
    Signed-off-by: Allwell Onen <aleenfestus@gmail.com>

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -1,7 +1,5 @@
 import "./About.css";
 
-import { Text, Title } from "@mantine/core";
-
 import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 import ProfileCard from "../ProfileCard/ProfileCard";
@@ -29,6 +27,10 @@ const About = () => {
   //   setEditorial(editorialAdvicers);
   // }, []);
 
+  const HEADERSTYLE = "title has-text-dark has-text-weight-bold is-4 mt-3";
+  const HEADERCOLOR = "has-text-link";
+  const ABOUT_TEXT = "is-text-5 has-text-dark";
+
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -38,31 +40,31 @@ const About = () => {
       <NavBar />
 
       <div className="about">
-        <Title className="aboutHead" order={1}>
-          About <span>TheCurrent</span>
-        </Title>
+        <h2 className={`${HEADERSTYLE} mt-5`}>
+          About <span className={HEADERCOLOR}>TheCurrent</span>
+        </h2>
 
-        <Text className="aboutText">
+        <p className={ABOUT_TEXT}>
           TheCurrent is a practice newspaper produced by Mass Communication
           students of Anchor University, Lagos. The maiden edition of the
           newspaper was a print edition produced in 2019. However, to keep up
           with global trends, we are introducing an online version from March,
           2022, in line with global trends.
-        </Text>
+        </p>
 
-        <Text className="aboutText">
+        <p className={ABOUT_TEXT}>
           As our name suggests, news should be really current to serve its
           purpose. Publishing our contents online helps us keep our news fresh,
           current and easily updated, thereby making The Current more timely and
           visible as people from all parts of the world access timely news from
           this website. We are here to serve you better round the clock.
-        </Text>
+        </p>
 
-        <Title className="head" order={1}>
-          Founding <span>Editors</span>
-        </Title>
+        <h2 className={`${HEADERSTYLE} mt-5`}>
+          Founding <span className={HEADERCOLOR}>Editors</span>
+        </h2>
 
-        <div className="editors">
+        <div className="about-sub-section">
           {/* {founding.map((profile) => {
           return <ProfileCard {...profile} key={profile.id} />;
         })} */}
@@ -88,11 +90,11 @@ const About = () => {
           />
         </div>
 
-        <Title className="head" order={1}>
-          Current <span>Editors</span>
-        </Title>
+        <h2 className={`${HEADERSTYLE} mt-5`}>
+          Current <span className={HEADERCOLOR}>Editors</span>
+        </h2>
 
-        <div className="editors">
+        <div className="about-sub-section">
           {/* {current.map((profile) => {
           return <ProfileCard {...profile} key={profile.id} />;
         })} */}
@@ -119,11 +121,11 @@ const About = () => {
           />
         </div>
 
-        <Title className="head" order={1}>
-          Editorial <span>Advicers</span>
-        </Title>
+        <h2 className={`${HEADERSTYLE} mt-5`}>
+          Editorial <span className={HEADERCOLOR}>Advicers</span>
+        </h2>
 
-        <div className="editors">
+        <div className="about-sub-section">
           {/* {editorial.map((profile) => {
           return (
             <ProfileCard
