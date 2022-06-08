15a4887133a466d416ede98536eb9304d9559be9
Allwell Onen <aleenfestus@gmail.com>
2022-06-08 10:25:56 +0100

    Pin a post

---
+++ build/precache-manifest.cc3421f710c9f7c3b51724f442968c79.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c897c5f5fd6e8e80a80e",
+    "url": "/static/css/main.a3ece86b.chunk.css"
+  },
+  {
+    "revision": "c897c5f5fd6e8e80a80e",
+    "url": "/static/js/main.c897c5f5.chunk.js"
+  },
+  {
+    "revision": "b50fe938aceea6252077",
+    "url": "/static/css/1.73b9b01b.chunk.css"
+  },
+  {
+    "revision": "b50fe938aceea6252077",
+    "url": "/static/js/1.b50fe938.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "693ad7e73a03c2d575a09096ddb518c3",
+    "url": "/static/media/anchor-logo.693ad7e7.png"
+  },
+  {
+    "revision": "c795c7108039fa07ceeb35a1ee2c1669",
+    "url": "/static/media/profileButtonImg.c795c710.png"
+  },
+  {
+    "revision": "725405c81e091fa5a7eabd8ea1fc33d3",
+    "url": "/static/media/pagenotfound.725405c8.svg"
+  },
+  {
+    "revision": "b3ee44b038e973672c6f80cc8e89f546",
+    "url": "/static/media/bg4.b3ee44b0.jpg"
+  },
+  {
+    "revision": "681c362475722dce83b76080948b8d1f",
+    "url": "/index.html"
+  }
+];
